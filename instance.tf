dd03deb30e83a695bd9af8d7012f7fdb7a0ddb9d
Shree16112021 <msagar21@gmail.com>
2025-03-24 19:10:41 +0530

    code update-removed instance

--- instance.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "main-instance" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public-subnet.id
-  tags = {
-    Name = "main-instance"
-  }
-}
